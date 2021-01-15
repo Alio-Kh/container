f7f8112b6da2150e502bdf7600b58a59f30d9d6d
Ali KHYATTI <56565367+Ali-kh99@users.noreply.github.com>
2021-01-15 18:10:05 +0100

    add usr/lib

--- build-root.py
+++ build-root.py
@@ -1,19 +1,22 @@
 import os
 
+# Create empty fs dirs 
 os.system("mkdir proc")
 os.system("mkdir bin")
 os.system("mkdir dev")
 os.system("mkdir lib")
 os.system("mkdir lib64")
 os.system("mkdir usr")
+os.system("mkdir usr/lib")
 
+# Copy fs dirs from the kernel
 os.system("sudo cp -r /bin/* ./bin/")
 print("bin is created")
 os.system("sudo cp -r /lib/* ./lib/")
 print("lib is created")
-os.system("sudo cp -dev/* ./dev")
-print("dev is created")
+#os.system("sudo cp -dev/* ./dev")
+#print("dev is created")
 os.system("sudo cp -r /lib64/* ./lib64/")
 print("lib64 is created")
-os.system("sudo cp -r /usr/* ./usr")
+os.system("sudo cp -r /usr/* ./usr/lib/")
 print("usr is created")
