29fd023bbdddf354beecca1c74379d275b05b8ee
Ali KHYATTI <56565367+Ali-kh99@users.noreply.github.com>
2021-01-15 18:19:29 +0100

    add commnets

--- hungry.c
+++ hungry.c
@@ -4,19 +4,19 @@
 #include <unistd.h>
 #include <string.h>
 
-int main(void)
-{
-            int i=0;
-            int *p;
-            while(1){
-                        i++;
-                        p= (int *)malloc(1000000);
-                        if(p)
-                                    printf("1MB allocated i=%d\n",i);
-                        else
-                                    printf("out of memory\n");
-                        memset(p,0,1000000);
-                       // sleep(1);
-       }
-       return 0;
+int main(void){
+	int i=0;
+	int *p;
+	while(1){
+	    i++;
+	    // Allocate 1Mb
+	    p= (int *)malloc(1000000);
+	    if(p)
+		printf("%dMB Allocated\n",i);
+	    else
+		printf("out of memory\n");
+	    //  Fill a block of memory with a particular value
+	    memset(p,0,1000000);
+	}
+	return 0;
 }
